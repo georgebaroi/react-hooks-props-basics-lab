4546e455e048892a84ad18c875aaca469e8f23da
George Baroi <baroi.george@gmail.com>
2022-08-12 14:15:27 -0400

    Done with assignment

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Links(props){
+return<h3>Links
+        <a href="{props.github}">https://github.com/liza</a>
+        <a href="{props.linkedin}">https://www.linkedin.com/in/liza/</a>
+      </h3>
+}
+
+
+export default Links
